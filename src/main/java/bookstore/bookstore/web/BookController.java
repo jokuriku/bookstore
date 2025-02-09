bce8f1852444b246c9ca79974ff261410e432e80
jokuriku <jokuriku@gmail.com>
2025-02-09 23:24:29 +0200

    add crud stuff

--- src/main/java/bookstore/bookstore/web/BookController.java
+++ src/main/java/bookstore/bookstore/web/BookController.java
@@ -6,24 +6,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import bookstore.bookstore.domain.Book;
 import bookstore.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
     @Autowired
-	BookRepository bookRepository; 
+    BookRepository bookRepository;
 
     @GetMapping({ "/index" })
     public String getIndex() {
         return "index";
     }
 
-    @GetMapping({ "/booklist"})
+    @GetMapping({ "/booklist" })
     public String getBooklist(Model model) {
-        List<Book> books =  (List<Book>) bookRepository.findAll(); // haetaan tietokannasta autot
-        model.addAttribute("books", books); // välitetään autolista templatelle model-olion avulla
+        List<Book> books = (List<Book>) bookRepository.findAll();
+        model.addAttribute("books", books);
         return "booklist";
+    }
+
+    // tyhjän autolomakkeen muodostaminen
+    @GetMapping("/addbook")
+    public String getNewBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/savebook")
+    public String saveBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/deletebook/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        bookRepository.deleteById(bookId);
+        return "redirect:/booklist";
     }
 }
