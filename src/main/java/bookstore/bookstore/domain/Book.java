7280f90fb3abb0cefd58633f02cea7a059767db2
jokuriku <jokuriku@gmail.com>
2025-02-09 22:23:17 +0200

    create @Entity from Book

--- src/main/java/bookstore/bookstore/domain/Book.java
+++ src/main/java/bookstore/bookstore/domain/Book.java
@@ -1,6 +1,15 @@
 package bookstore.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String author;
     private String isbn;
     private double price;
